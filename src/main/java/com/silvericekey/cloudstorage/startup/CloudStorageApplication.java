ea1a8a25e09823e9e43864eb1f4d422a89fc63ea
SilverIceKey <z516798599@gmail.com>
2021-12-26 10:22:35 +0800

    plumelog 集成

--- src/main/java/com/silvericekey/cloudstorage/startup/CloudStorageApplication.java
+++ src/main/java/com/silvericekey/cloudstorage/startup/CloudStorageApplication.java
@@ -1,4 +1,4 @@
-package com.silvericekey.cloudstorage;
+package com.silvericekey.cloudstorage.startup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
