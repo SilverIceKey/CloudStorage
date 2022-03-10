043d811109dfa1c76779124f8e05ce7b114ab4c5
SilverIceKey <z516798599@gmail.com>
2022-03-10 16:22:40 +0800

    测试修改

--- src/test/java/com/silvericekey/cloudstorage/util/EncryptUtilTest.java
+++ src/test/java/com/silvericekey/cloudstorage/util/EncryptUtilTest.java
@@ -14,23 +14,4 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class EncryptUtilTest {
 
-    @Test
-    void encrypt() {
-        assertEquals(EncryptUtil.encrypt("123456"),"kvepcuu+yM2srPoYFjGySQ==");
-    }
-
-    @Test
-    void decrypt() {
-        assertEquals(EncryptUtil.decrypt("kvepcuu+yM2srPoYFjGySQ=="),"123456");
-    }
-
-    @Test
-    void md5() {
-        assertEquals(EncryptUtil.md5("123456"),"e10adc3949ba59abbe56e057f20f883e");
-    }
-
-    @Test
-    void testMd5() {
-        assertEquals(EncryptUtil.md5(new File("E:\\project\\涂鸦设备信息.txt")).toUpperCase(),"B9BC7F7C21760348D7B04C27A7ACED7B");
-    }
 }
